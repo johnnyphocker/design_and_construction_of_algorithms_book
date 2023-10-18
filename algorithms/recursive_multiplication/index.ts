602c21f0c92fd5079458d9f60d972f9ba04c29dd
Juan Inzunza <johnny.phocker@gmail.com>
2023-10-18 10:37:12 -0700

    Added recursive multiplication algorithm

--- algorithms/recursive_multiplication/index.ts
+++ algorithms/recursive_multiplication/index.ts
@@ -1,3 +1,10 @@
-exports.recursive_multiplication = () => {
-  
+exports.recursive_multiplication = (x: number, y: number): number => {
+  if (x === 0 || y === 0) return 0;
+  if (x < 0) {
+    return -this.recursive_multiplication(-x, y);
+  }
+  if (y < 0) {
+    return -this.recursive_multiplication(x, -y);
+  }
+  return x + this.recursive_multiplication(x, y - 1);
 }
