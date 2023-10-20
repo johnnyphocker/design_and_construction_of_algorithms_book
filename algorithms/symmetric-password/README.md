9cfdca3e2d1798c23617ff100e60f3ad393fb954
Juan Inzunza <johnny.phocker@gmail.com>
2023-10-20 13:21:07 -0700

    Added symmetric passowrd algorithm

---
+++ algorithms/symmetric-password/README.md
@@ -0,0 +1,5 @@
+## SYMMETRIC PASSWORD
+
+Write a function that takes an array of integers containing 0 and 1 as input and returns “Yes” or “No” depending on whether the given password is symmetric or not.
+
+`symmetric_password([1, 1, 0, 0, 0, 0, 0, 1, 1]) // 'Yes'`
---
+++ algorithms/symmetric-password/index.ts
@@ -0,0 +1,13 @@
+exports.symmetric_password = (arr: number[]): string => {
+  const half: number = Math.floor(arr.length / 2);
+  const left_half: number[] = arr.slice(0, half);
+  const right_half: number[] = arr.reverse().slice(0, half);
+
+  for (let i = 0; i < half; i++) {
+    if (left_half[i] !== right_half[i]) {
+      return 'No';
+    }
+  }
+
+  return 'Yes';
+}
